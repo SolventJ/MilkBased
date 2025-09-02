18faea4c1129afa14f3eedb88d99bacc2e4f2c69
SolventJ <70345684+SolventJ@users.noreply.github.com>
2025-09-02 06:47:36 +0300

    first

---
+++ src/main/java/com/github/solventj/milkbased/MilkBased.java
@@ -0,0 +1,37 @@
+package com.github.solventj.milkbased;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(MilkBased.MOD_ID)
+public class MilkBased {
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "milkbased";
+
+    public MilkBased(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for mod loading
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
